27c04531ff061315df6dc7039278e48fff84b74b
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-17 02:03:24 +0300

    joyeuse: Fix vendor.qti.imsrtpservice@3.0 ELF checks
    Change-Id: Ia1995d0a3360317843685082e3226ab30f124d09
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- sm6250-common-vendor.mk
+++ sm6250-common-vendor.mk
@@ -809,7 +809,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.ims.rcsconfig@2.0 \
     vendor.qti.ims.rcsconfig@2.1 \
     vendor.qti.imsrtpservice@3.0-service-Impl \
-    vendor.qti.imsrtpservice@3.0 \
+    vendor.qti.imsrtpservice@3.0_vendor \
     vendor.qti.latency@2.0 \
     vendor.qti.latency@2.1 \
     capi_v2_aptX_CLHDADV_Encoder \
