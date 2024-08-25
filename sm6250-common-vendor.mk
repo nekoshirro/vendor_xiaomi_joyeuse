260bbdc6bc74f89d7707fb4fcacf125be2fa48d8
Michael Bestas <mkbestas@lineageos.org>
2024-08-25 08:30:58 +0300

    joyeuse: fix vendor.qti.hardware.fm@1.0 ELF checks
    Change-Id: I43f0824f88c24dd9d039f9075090c17f4266ab2d
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- sm6250-common-vendor.mk
+++ sm6250-common-vendor.mk
@@ -765,7 +765,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.data.lce@1.0 \
     vendor.qti.hardware.data.qmi@1.0 \
     vendor.qti.hardware.dsp@1.0 \
-    vendor.qti.hardware.fm@1.0 \
+    vendor.qti.hardware.fm@1.0_vendor \
     vendor.qti.hardware.mwqemadapter@1.0 \
     vendor.qti.hardware.qccsyshal@1.0 \
     vendor.qti.hardware.radio.am@1.0 \
