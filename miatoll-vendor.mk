45ee89d945bbdbfcf52a5ca652d7788e60015510
COSMIC <rayavanindra@gmail.com>
2025-04-14 10:47:53 +0530

    joyeuse: Update WFD system blobs from dodge Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- miatoll-vendor.mk
+++ miatoll-vendor.mk
@@ -901,6 +901,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.diaghal@1.0 \
     vendor.qti.hardware.fm@1.0 \
     vendor.qti.hardware.wifidisplaysession@1.0 \
+    vendor.qti.hardware.wifidisplaysession_aidl-V1-ndk \
     vendor.qti.imsrtpservice@3.0 \
     CACertService \
     CneApp \
